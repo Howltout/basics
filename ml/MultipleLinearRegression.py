732a10495e07bbdc46e06fc95dbfe411edb3e849
Tanuj Katyal <126747122+Howltout@users.noreply.github.com>
2023-05-25 21:23:17 +0530

    Update MultipleLinearRegression.py

--- ml/MultipleLinearRegression.py
+++ ml/MultipleLinearRegression.py
@@ -1,4 +1,16 @@
 import pandas as pd
 import numpy as np
+import math
 from sklearn importlinear_model
 
+df = pd.read_csv('homeprices.csv')
+bed_median = math.floor(df.bedrooms.median())
+df.bedrooms = df.bedrooms.filna(bed_median)
+
+reg = linear_model.LinearRegression()
+reg.fit(df[['area','bedrooms','age']],df.price)
+
+reg.coef_
+reg.intercept_
+
+reg.predict([4000,3,35]
